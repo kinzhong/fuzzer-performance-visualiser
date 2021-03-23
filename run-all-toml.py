6935ca2c53c97d1d1dda2aa0edd3332dff80f73d
Chew Kin Zhong <45738763+kinzhong@users.noreply.github.com>
2021-03-23 18:32:25 +0800

    Delete run-all-toml.py

--- run-all-toml.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = r'D:\Users\KinZhong\Dropbox\Computer Science\Final Year Project\fuzzer-performance-visualiser\toml-files'
-for filename in os.listdir(directory):
-    print("sample-toml\\" + filename)
-    os.system("python3 visualiser.py sample-toml\\" + filename)
-
-
-
